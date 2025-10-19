2ff2124b498cabb5063d9c9354c44ac2ceda7ada
Курек Семён Евгеньевич <skurek@forus.ru>
2025-10-19 14:50:00 +0800

    Initial installation

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { ThemeToggler } from "@/components/theme-toggler";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <ThemeToggler />
+    </div>
+  );
+}
